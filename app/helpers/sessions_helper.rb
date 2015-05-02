7d772dd4bb64956a6a47e9c139bbdb2091477f2c
duytsev <duytsev@gmail.com>
2015-05-02 22:21:14 +0300

    added logging in and tests

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,20 @@
+module SessionsHelper
+
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  def log_out(user)
+    session[user.id] = nil
+    render 'new'
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+end
