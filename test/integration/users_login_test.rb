30e2a50de5fb4ae8ea6a80e08bcdc045ecd37d93
duytsev <duytsev@gmail.com>
2015-05-03 15:21:41 +0300

    added tests for remember me

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -38,4 +38,14 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", user_path(@user), count: 0
   end
 
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    assert_not_nil cookies['remember_token']
+  end
+
+  test "login without remembering" do
+    log_in_as(@user, remember_me: '0')
+    assert_nil cookies['remember_token']
+  end
+
 end
