3ad9986b07621c2e4dc55367e14f100421eb294d
duytsev <duytsev@gmail.com>
2015-05-17 13:26:44 +0300

    added micropost model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :microposts, dependent: :destroy
   attr_accessor :remember_token, :activation_token, :reset_token
 
   before_save :downcase_email
@@ -73,6 +74,7 @@ class User < ActiveRecord::Base
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
   end
+
 
   private
 
